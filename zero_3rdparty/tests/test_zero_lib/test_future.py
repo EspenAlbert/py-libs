9dd5d53a558e887956fca7fe227e16f803c6e034
EspenAlbert <espen@wheel.me>
2023-04-25 06:49:14 +0100

    test: type hints tests for Future[str]

--- zero_3rdparty/tests/test_zero_lib/test_future.py
+++ zero_3rdparty/tests/test_zero_lib/test_future.py
@@ -7,7 +7,16 @@ def test_type_completion():
     def return_string() -> str:
         return "OK"
 
+    def return_string_by_arg(a: str) -> str:
+        return a
+
     with ThreadPoolExecutor() as executor:
+        # pycharm is flagging this as an error
         future: Future[str] = executor.submit(return_string)
         result = safe_wait(future)
         assert result == "OK"
+
+        # pycharm is not flagging this as an error
+        future2: Future[str] = executor.submit(return_string_by_arg, "OK2")
+        result2 = safe_wait(future2)
+        assert result2 == "OK2"

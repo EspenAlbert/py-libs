f652ce811d6fc1e8e802a2c0031e187872a9e81a
EspenAlbert <espen@wheel.me>
2023-05-02 07:50:46 +0100

    test: remove flaky test

--- zero_3rdparty/tests/test_zero_lib/test_datetime_utils.py
+++ zero_3rdparty/tests/test_zero_lib/test_datetime_utils.py
@@ -7,10 +7,3 @@ from zero_3rdparty.datetime_utils import date_filename, utc_now_ms_precision
 @pytest.mark.freeze_time("2020-01-01T08:34:22.124151")
 def test_values_time_name():
     assert date_filename() == "2020-01-01T08-34Z"
-
-
-def test_utc_now_ms():
-    prev_date = utc_now_ms_precision()
-    time.sleep(0.1)
-    now = utc_now_ms_precision()
-    assert prev_date <= now

9e21c9ec96251b7f0b9479944a9b6f15d8f25dce
EspenAlbert <albertespen@gmail.com>
2023-08-23 14:31:21 +0100

    docs: add some examples to the model_lib docs

--- model_lib/test/test_model_lib/test_readme_example.py
+++ model_lib/test/test_model_lib/test_readme_example.py
@@ -1,18 +1,38 @@
 from datetime import datetime
+from typing import Union
 
 from freezegun import freeze_time
 from pydantic import Field
 
-from model_lib import Entity, Event, dump, dump_with_metadata, parse_model
+from model_lib import (
+    Entity,
+    Event,
+    FileFormat,
+    dump,
+    dump_with_metadata,
+    parse_model,
+    register_dumper,
+)
+from model_lib.serialize.parse import parse_payload, register_parser
+
+dump_formats = list(FileFormat)
+expected_dump_formats: list[str] = [
+    "json",
+    "pretty_json",
+    "yaml",
+    "yml",
+    "json_pydantic",
+    "pydantic_json",
+    "toml",
+    "toml_compact",
+]
+missing_dump_formats = set(FileFormat) - set(expected_dump_formats)
+assert not missing_dump_formats, f"found missing dump formats: {missing_dump_formats}"
 
 
 class Birthday(Event):
     """
     >>> birthday = Birthday()
-    >>> birthday.date = datetime.utcnow()
-    Traceback (most recent call last):
-    ...
-    TypeError: "Birthday" is immutable and does not support item assignment
     """
 
     date: datetime = Field(default_factory=datetime.utcnow)
@@ -65,3 +85,43 @@ def test_show_parsing(tmp_path):
     path_yaml = tmp_path / "example.yaml"
     path_yaml.write_text(_metadata_dump)
     assert parse_model(path_yaml) == person  # metadata is used to find the class
+
+
+class CustomDumping:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __eq__(self, other):
+        if isinstance(other, CustomDumping):
+            return self.__dict__ == other.__dict__
+        return super().__eq__(other)
+
+
+def custom_dump(custom: CustomDumping):
+    return dict(full_name=f"{custom.first_name} {custom.last_name}")
+
+
+register_dumper(CustomDumping, custom_dump)
+
+
+class CustomKafkaPayload:
+    def __init__(self, body: str, topic: str):
+        self.topic = topic
+        self.body = body
+
+
+def custom_parse_kafka(payload: CustomKafkaPayload, format: str) -> Union[dict, list]:
+    return parse_payload(payload.body, format)
+
+
+register_parser(CustomKafkaPayload, custom_parse_kafka)
+
+
+def test_custom_dump():
+    instance = CustomDumping("Espen", "Python")
+    assert dump(instance, "json") == '{"full_name":"Espen Python"}'
+    payload = CustomKafkaPayload(
+        body='{"first_name": "Espen", "last_name": "Python"}', topic="some-topic"
+    )
+    assert parse_model(payload, t=CustomDumping) == instance

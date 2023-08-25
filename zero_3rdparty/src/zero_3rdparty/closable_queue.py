1833444a7f91975171d613fee15002f7e4f65237
EspenAlbert <albertespen@gmail.com>
2023-08-25 07:57:11 +0100

    fix(closable_queue): guarantee no put is called after close

--- zero_3rdparty/src/zero_3rdparty/closable_queue.py
+++ zero_3rdparty/src/zero_3rdparty/closable_queue.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import logging
+import threading
 from collections.abc import Iterable
 from contextlib import suppress
 from functools import partial
 from queue import Empty, Queue
+from threading import RLock
 from typing import Generic, TypeVar
 
 logger = logging.getLogger(__name__)
@@ -31,10 +33,17 @@ class ClosableQueue(Queue[QueueType], Generic[QueueType]):
     def __init__(self, maxsize: int = 0):  # 0 == infinite
         super().__init__(maxsize=maxsize)
         self.__QUEUES.append(self)
+        # ensure close doesn't block and can set raise error
+        self.mutex = RLock()  # type: ignore
+
+        self.not_empty = threading.Condition(self.mutex)
+        self.not_full = threading.Condition(self.mutex)
+        self.all_tasks_done = threading.Condition(self.mutex)
 
     def close(self):
-        self.put(self.SENTINEL)
-        self.put = partial(_raise_queue_is_closed, queue=self)
+        with self.mutex:
+            self.put(self.SENTINEL)
+            self.put = partial(_raise_queue_is_closed, queue=self)
         with suppress(Exception):
             self.__QUEUES.remove(self)
 
@@ -43,25 +52,21 @@ class ClosableQueue(Queue[QueueType], Generic[QueueType]):
             self.close()
 
     def __iter__(self) -> Iterable[QueueType]:
-        while True:
-            item = self.get()
-            try:
+        try:
+            while True:
+                item = super().get(block=True)
                 if item is self.SENTINEL:
-                    self.put(item)  # ensure next iterator will finish immediately
+                    with self.mutex:
+                        # ensure next iterator will finish immediately
+                        self.queue.append(item)
+                        self.not_empty.notify()
                     return  # Cause the thread to exit
                 yield item
-            finally:
+        except BaseException as e:
+            logger.exception(e)
+        finally:
+            with suppress(ValueError):
                 self.task_done()
-
-    def has_next(self):
-        """
-        Warning: Side effect of put first item to the end of the queue
-        """
-        next = self.pop()
-        if next is _empty:
-            return False
-        self.put(next)
-        return True
 
     def pop(self, default=_empty):
         try:
@@ -87,5 +92,13 @@ class ClosableQueue(Queue[QueueType], Generic[QueueType]):
     def get_nowait(self) -> QueueType:
         item = super().get_nowait()
         if item is self.SENTINEL:
+            self.queue.append(item)
+            raise QueueIsClosed
+        return item
+
+    def get(self, block: bool = True, timeout: float | None = None) -> QueueType:
+        item = super().get(block, timeout)
+        if item is self.SENTINEL:
+            self.queue.append(self.SENTINEL)
             raise QueueIsClosed
         return item

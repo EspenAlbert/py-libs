a211e32f9f6fa44693db5d88c7db2d10708b21f6
EspenAlbert <albertespen@gmail.com>
2025-06-04 09:06:13 +0100

    feat: support GhExtExport for storing variables and secrets in static dir

--- scripts/gh_ext.py
+++ scripts/gh_ext.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import logging
 import re
-from functools import cached_property, lru_cache
+from dataclasses import dataclass
+from datetime import datetime
+from functools import cached_property, lru_cache, total_ordering, wraps
 from pathlib import Path
-from typing import Callable, ClassVar, Self
+from typing import Any, Callable, ClassVar, Generic, Protocol, Self, TypeVar
 
 import git
 import typer
@@ -13,12 +15,17 @@ from ask_shell._run_env import interactive_shell
 from ask_shell.interactive2 import select_list_multiple
 from ask_shell.rich_live import print_to_live_console
 from ask_shell.rich_progress import new_task
+from ask_shell.settings import AskShellSettings
 from ask_shell.typer_command import configure_logging
 from model_lib import Entity, parse_payload
-from pydantic import BaseModel, Field, RootModel
+from model_lib.serialize import dump
+from model_lib.serialize.parse import parse_model
+from model_lib.static_settings import StaticSettings
+from pydantic import BaseModel, ConfigDict, Field, RootModel, model_validator
 from pydantic_core import Url
 from rich.markdown import Markdown
 from zero_3rdparty import dict_nested, file_utils, iter_utils
+from zero_3rdparty.datetime_utils import utc_now
 
 
 def find_owner_project(repo: git.Repo) -> str:
@@ -52,13 +59,157 @@ def ensure_yaml_parsing_bool_parsing_works_as_expected():
             ]
 
 
-class GhVar(BaseModel):
+class GhExtSettings(StaticSettings):
+    def repo_files_dir(self, owner_project: str) -> Path:
+        return self.static_root / owner_project
+
+    def repo_export_path(self, owner_project: str) -> Path:
+        return self.repo_files_dir(owner_project) / "gh-ext-export.yaml"
+
+
+@total_ordering
+class GhVarOrSecret(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
     name: str
+    updated_at: datetime = Field(alias="updatedAt")
+    deleted: bool = False
+    deleted_ts: datetime | None = None
+
+    def mark_deleted(self) -> None:
+        if self.deleted:
+            return  # already deleted
+        self.deleted = True
+        self.deleted_ts = utc_now()
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, GhVarOrSecret):
+            raise TypeError(f"Cannot compare {type(self)} with {type(other)}")
+        return (self.deleted, self.name) < (other.deleted, other.name)
+
+    def update(self, other: GhVarOrSecret) -> None:
+        if self.name != other.name:
+            raise ValueError(
+                f"Cannot update {self.__class__.__name__} {self.name} with {other.name}"
+            )
+        self.updated_at = other.updated_at
+
+    @classmethod
+    def get_changes(
+        cls, old: list[GhVarOrSecretT], new: list[GhVarOrSecretT]
+    ) -> GhVarOrSecretChanges[GhVarOrSecretT]:
+        old_dict = var_list_to_dict(old)
+        new_dict = var_list_to_dict(new)
+        return GhVarOrSecretChanges(
+            added=[value for name, value in new_dict.items() if name not in old_dict],
+            updated=[
+                new_var
+                for name, new_var in new_dict.items()
+                if name in old_dict and old_dict[name] != new_var
+            ],
+            deleted=[
+                old_var for name, old_var in old_dict.items() if name not in new_dict
+            ],
+        )
+
+
+GhVarOrSecretT = TypeVar("GhVarOrSecretT", bound=GhVarOrSecret)
+
+
+@dataclass
+class GhVarOrSecretChanges(Generic[GhVarOrSecretT]):
+    added: list[GhVarOrSecretT]
+    updated: list[GhVarOrSecretT]
+    deleted: list[GhVarOrSecretT]
+
+
+def var_list_to_dict(some_list: list[GhVarOrSecretT]) -> dict[str, GhVarOrSecretT]:
+    return {var.name: var for var in some_list}
+
+
+class GhVar(GhVarOrSecret):
     value: str
 
+    def update(self, other: GhVarOrSecret) -> None:
+        assert isinstance(other, GhVar)
+        super().update(other)
+        self.value = other.value
 
-class GhSecret(BaseModel):
-    name: str
+
+class GhSecret(GhVarOrSecret):
+    pass
+
+
+class UpdateMethod(Protocol):
+    def __call__(self, self_: GhExtExport, *args: Any, **kwargs: Any) -> None: ...
+
+
+T = TypeVar("T", bound=Callable)
+
+
+def ensure_disk_path_updated(func: T) -> T:
+    @wraps(func)
+    def wrapper(self_: GhExtExport, *args: Any, **kwargs: Any) -> Any:
+        try:
+            return func(self_, *args, **kwargs)
+        except BaseException as e:
+            raise e
+        finally:
+            file_ext = self_.disk_path.suffix
+            assert file_ext in (".yaml", ".yml"), (
+                "Disk path must have .yaml or .yml extension"
+            )
+            yaml_text = dump(self_, "yaml")
+            file_utils.ensure_parents_write_text(self_.disk_path, yaml_text)
+
+    return wrapper  # type: ignore
+
+
+class GhExtExport(BaseModel):
+    variables: list[GhVar] = Field(default_factory=list)
+    secrets: list[GhSecret] = Field(default_factory=list)
+    disk_path: Path
+
+    @ensure_disk_path_updated
+    def update_variables(self, new_variables: list[GhVar]) -> None:
+        self._sync_list_changes(self.variables, new_variables)
+
+    def _sync_list_changes(
+        self, exported_list: list[GhVarOrSecretT], new_list: list[GhVarOrSecretT]
+    ):
+        changes = GhVarOrSecret.get_changes(exported_list, new_list)
+        old_dict = var_list_to_dict(exported_list)
+        if added := changes.added:
+            exported_list.extend(added)
+        if deleted := changes.deleted:
+            for var in deleted:
+                old_var = old_dict[var.name]
+                old_var.mark_deleted()
+        if updated := changes.updated:
+            for var in updated:
+                old_var = old_dict[var.name]
+                old_var.update(old_var)
+
+    @ensure_disk_path_updated
+    def update_secrets(self, new_secrets: list[GhSecret]) -> None:
+        self._sync_list_changes(self.secrets, new_secrets)
+
+    @ensure_disk_path_updated
+    def mark_deleted_secret(self, name: str) -> None:
+        """Mark a secret as deleted by setting its 'deleted' attribute to True."""
+        for secret in self.secrets:
+            if secret.name == name:
+                secret.mark_deleted()
+                return
+        raise ValueError(f"Secret with name {name} not found in the export.")
+
+    @ensure_disk_path_updated
+    def mark_deleted_variable(self, name: str) -> None:
+        """Mark a variable as deleted by setting its 'deleted' attribute to True."""
+        for var in self.variables:
+            if var.name == name:
+                var.mark_deleted()
+                return
+        raise ValueError(f"Variable with name {name} not found in the export.")
 
 
 class GhVars(RootModel):
@@ -68,6 +219,10 @@ class GhVars(RootModel):
     @cached_property
     def names(self) -> set[str]:
         return {var.name for var in self.root}
+
+    def unused_dict(self, unused: list[str]) -> dict[str, str]:
+        """Returns a dictionary of unused variables with their values."""
+        return {var.name: var.value for var in self.root if var.name in unused}
 
 
 class GhSecrets(RootModel):
@@ -120,10 +275,11 @@ def gh_vars_usage(
     if print_report or delete_unused:
         print_to_live_console(Markdown(result.report_or_ok))
     if unused_vars := result.unused_vars:
+        unused_var_names = list(unused_vars)
         vars_to_delete = select_list_multiple(
             "Select unused variables to delete",
-            choices=unused_vars,
-            default=unused_vars,
+            choices=unused_var_names,
+            default=unused_var_names,
         )
         delete_vars(vars_to_delete, ctx)
     if unused_secrets := result.unused_secrets:
@@ -139,6 +295,7 @@ def delete_vars(vars_to_delete: list[str], ctx: CreateVariableSecretReportContex
     for var_name in vars_to_delete:
         command = f"gh variable delete -R {ctx.owner_project} {var_name}"
         run_and_wait(command, cwd=ctx.repo_path, user_input=True)
+        ctx.export.mark_deleted_variable(var_name)
 
 
 def delete_secrets(
@@ -147,6 +304,7 @@ def delete_secrets(
     for secret_name in secrets_to_delete:
         command = f"gh secret delete -R {ctx.owner_project} {secret_name}"
         run_and_wait(command, cwd=ctx.repo_path, user_input=True)
+        ctx.export.mark_deleted_secret(secret_name)
 
 
 class CreateVariableSecretReportContext(Entity):
@@ -155,6 +313,19 @@ class CreateVariableSecretReportContext(Entity):
     repo_path: Path
     call_path: Path
     owner_project: str
+    shell_settings: AskShellSettings = Field(default_factory=AskShellSettings.from_env)
+    gh_ext_settings: GhExtSettings = Field(default_factory=GhExtSettings.from_env)
+    export: GhExtExport = Field(init=False, default=None)  # type: ignore
+
+    @model_validator(mode="after")
+    def read_or_create_export(self) -> Self:
+        owner_project = self.owner_project
+        export_path = self.gh_ext_settings.repo_export_path(owner_project)
+        if export_path.exists():
+            self.export = parse_model(export_path, GhExtExport)
+        else:
+            self.export = GhExtExport(disk_path=export_path)
+        return self
 
     @cached_property
     def repo(self) -> git.Repo:
@@ -189,7 +360,7 @@ class CreateVariableSecretReportContext(Entity):
 
 class CreateVariableSecretReportContextOutput(BaseModel):
     report_md: str = ""
-    unused_vars: list[str] = Field(default_factory=list)
+    unused_vars: dict[str, str] = Field(default_factory=dict)
     unused_secrets: list[str] = Field(default_factory=list)
 
     @property
@@ -211,18 +382,24 @@ def create_variable_secret_report(
     report_md: list[str] = []
     out_event = CreateVariableSecretReportContextOutput()
 
-    vars_out = run_and_wait(f"gh variable list -R {owner_project} --json name,value")
+    vars_out = run_and_wait(
+        f"gh variable list -R {owner_project} --json name,value,updatedAt"
+    )
     vars_json = parse_payload(vars_out.stdout, "json")
     if vars_json:
         gh_vars = GhVars(root=vars_json)  # type: ignore
+        ctx.export.update_variables(gh_vars.root)
         vars_lines, unused_vars = create_md_usage_report(ctx, gh_vars, find_vars_usages)
-        out_event.unused_vars = unused_vars
+        out_event.unused_vars = gh_vars.unused_dict(unused_vars)
         report_md.extend(vars_lines)
     else:
         logger.warning("No variables found")
-    secrets_out = run_and_wait(f"gh secret list -R {owner_project} --json name")
+    secrets_out = run_and_wait(
+        f"gh secret list -R {owner_project} --json name,updatedAt"
+    )
     if secrets_json := parse_payload(secrets_out.stdout, "json"):
         gh_secrets = GhSecrets(root=secrets_json)  # type: ignore
+        ctx.export.update_secrets(gh_secrets.root)
         if report_md:
             report_md.append("\n\n")  # add extra new lines between vars and secrets
         secret_lines, unused_secrets = create_md_usage_report(

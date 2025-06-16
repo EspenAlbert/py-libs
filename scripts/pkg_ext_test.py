9aafe8868caacc625dd67c60fc1e4b4102e13ad3
EspenAlbert <albertespen@gmail.com>
2025-06-16 09:19:48 +0100

    feat: enhance pkg_ext and pkg_ext_test to support reference exposure and write init

--- scripts/pkg_ext_test.py
+++ scripts/pkg_ext_test.py
@@ -1,17 +1,14 @@
 from pathlib import Path
 
-from ask_shell import models, settings
+from ask_shell import _run_env, models, settings
 from pkg_ext import PkgSrcFile, create_refs, parse_symbols
 
 ASK_SHELL_PKG_IMPORT_NAME = "ask_shell"
 
 
 def test_parse_symbols():
+    symbols = _parse_src_module(settings)
     settings_path = Path(settings.__file__)
-    symbols = parse_symbols(
-        settings_path, settings_path.name, ASK_SHELL_PKG_IMPORT_NAME
-    )
-    assert isinstance(symbols, PkgSrcFile)
     assert symbols.path == settings_path
     assert symbols.relative_path == settings_path.name
     assert symbols.local_imports == {"ask_shell._run_env:interactive_shell"}
@@ -20,11 +17,7 @@ def test_parse_symbols():
 
 
 def test_parse_type_aliases():
-    models_path = Path(models.__file__)
-    symbols = parse_symbols(models_path, models_path.name, ASK_SHELL_PKG_IMPORT_NAME)
-    assert isinstance(symbols, PkgSrcFile)
-    assert symbols.path == models_path
-    assert symbols.relative_path == models_path.name
+    symbols = _parse_src_module(models)
     assert "ask_shell.settings:AskShellSettings" in symbols.local_imports
     assert "OutputCallbackT" in symbols.type_aliases
     assert "ShellRunEventT" in symbols.type_aliases
@@ -58,16 +51,8 @@ def test_parse_type_aliases():
 
 
 def test_create_refs():
-    models_path = Path(models.__file__)
-    models_parsed = parse_symbols(
-        models_path, models_path.name, ASK_SHELL_PKG_IMPORT_NAME
-    )
-    assert isinstance(models_parsed, PkgSrcFile)
-    settings_path = Path(settings.__file__)
-    settings_parsed = parse_symbols(
-        settings_path, settings_path.name, ASK_SHELL_PKG_IMPORT_NAME
-    )
-    assert isinstance(settings_parsed, PkgSrcFile)
+    models_parsed = _parse_src_module(models)
+    settings_parsed = _parse_src_module(settings)
     refs = create_refs([models_parsed, settings_parsed], ASK_SHELL_PKG_IMPORT_NAME)
     assert refs
     settings_ref = "ask_shell.settings:AskShellSettings"
@@ -76,12 +61,15 @@ def test_create_refs():
     assert actual_ref.src_usages == ["models.py"]
 
 
-def test_parse_symbols_run_env():
-    from ask_shell import _run_env
+def _parse_src_module(module) -> PkgSrcFile:
+    models_path = Path(module.__file__)
+    result = parse_symbols(models_path, models_path.name, ASK_SHELL_PKG_IMPORT_NAME)
+    assert isinstance(result, PkgSrcFile)
+    return result
 
-    run_env_path = Path(_run_env.__file__)
-    symbols = parse_symbols(run_env_path, run_env_path.name, ASK_SHELL_PKG_IMPORT_NAME)
-    assert isinstance(symbols, PkgSrcFile)
+
+def test_parse_symbols_run_env():
+    symbols = _parse_src_module(_run_env)
     assert symbols.functions == ["interactive_shell"]
     all_symbols = list(symbols.iterate_ref_symbols())
     symbols = {symbol.name: symbol for symbol in all_symbols}

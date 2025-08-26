94328cf3b6c84cd8124c355a92d62da020ae333b
EspenAlbert <albertespen@gmail.com>
2025-08-26 20:05:06 +0100

    test: Add bump tests

--- pkg-ext/pkg_ext/version_bump_test.py
+++ pkg-ext/pkg_ext/version_bump_test.py
@@ -1,14 +1,51 @@
+import pytest
+from zero_3rdparty.file_utils import ensure_parents_write_text
+
 from pkg_ext.conftest import TEST_PKG_NAME
+from pkg_ext.gen_changelog import ChangelogAction, ChangelogActionType
 from pkg_ext.models import PkgCodeState, PkgExtState, pkg_ctx
 from pkg_ext.version_bump import PkgVersion, bump_or_get_version
 
 
-def test_bump_or_get_version_default(settings):
-    ctx = pkg_ctx(
+@pytest.fixture()
+def pkg_ctx_instance(settings) -> pkg_ctx:
+    return pkg_ctx(
         settings,
         PkgExtState.parse(settings),
         PkgCodeState.model_construct(
             pkg_import_name=TEST_PKG_NAME, import_id_refs={}, files=[]
         ),
     )
-    assert bump_or_get_version(ctx) == PkgVersion.default()
+
+
+def test_bump_or_get_version_default(pkg_ctx_instance):
+    assert bump_or_get_version(pkg_ctx_instance) == PkgVersion.default()
+
+
+def test_bump_or_get_version_default_from_toml(settings, pkg_ctx_instance):
+    pyproject_toml = settings.pyproject_toml
+    ensure_parents_write_text(pyproject_toml, '[project]\nversion = "1.2.3"')
+    assert str(bump_or_get_version(pkg_ctx_instance)) == "1.2.3"
+
+
+_actions = [
+    (
+        ChangelogAction(name="func_name", type=ChangelogActionType.BREAKING_CHANGE),
+        "1.0.0",
+    ),
+    (ChangelogAction(name="func_name", type=ChangelogActionType.EXPOSE), "0.1.0"),
+    (ChangelogAction(name="func_name", type=ChangelogActionType.FIX), "0.0.2"),
+    (ChangelogAction(name="func_name", type=ChangelogActionType.HIDE), "0.0.1"),
+]
+
+
+@pytest.mark.parametrize(
+    "action,new_version", _actions, ids=[action.type for action, _ in _actions]
+)
+def test_bump_major(pkg_ctx_instance, action, new_version):
+    actions = [
+        ChangelogAction(name="0.0.1", type=ChangelogActionType.RELEASE),
+        action,
+    ]
+    pkg_ctx_instance._actions = actions
+    assert str(bump_or_get_version(pkg_ctx_instance)) == new_version
